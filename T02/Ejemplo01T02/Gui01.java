e07bdd06903f09cd4aa2bc4a229032d0c8369120
Aitor Rodriguez <Aitor.Gallardo.14@gmail.com>
2022-10-07 16:14:46 +0200

    Ejemplo de ventana con boton

---
+++ T02/Ejemplo01T02/Gui01.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Gui01 extends JFrame {
+    private Container panel;
+    private JButton miBtn;
+    private JButton miBtn2;
+    private JButton miBtn3;
+
+
+    public Gui01() {
+        super("Ventana con un boton");
+        miBtn = new JButton("Aceptar");
+        miBtn2 = new JButton("Cancelar");
+        miBtn3 = new JButton("Salir");
+        panel = getContentPane();
+        panel.add(miBtn);
+        panel.add(miBtn2);
+        panel.add(miBtn3);
+
+        setSize(200,100);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        Gui01 gui01 = new Gui01();
+    }
+}
