e4b374dc849fdd53befbba8e61eee96f3c2d6732
Aitor Rodriguez <Aitor.Gallardo.14@gmail.com>
2022-10-07 19:13:45 +0200

    Ejercicio 2 hecha parte 2

--- T02/P02/Ej02P02.java
+++ T02/P02/Ej02P02.java
@@ -1,19 +1,29 @@
 import javax.swing.*;
+import java.util.Scanner;
 
-public class Ej02P02 {
-    public Ej02P02() {
-        JFrame frame = new JFrame("hola");
-        JButton myBtn = new JButton("Adios");
-
-        frame.setSize(300, 300);
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
-        frame.getContentPane().add(myBtn);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+public class Ej02P02 extends JFrame {
+    public Ej02P02(String window, String btn)  {
+        super(window);
+        JButton myBtn = new JButton(btn);
+        setSize(300, 300);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        getContentPane().add(myBtn);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
-        Ej02P02 frame = new Ej02P02();
+        Ej02P02 frame = new Ej02P02("Hola", "Adios");
+        Scanner sc = new Scanner(System.in);
+        String window, btn;
 
+        System.out.println("Escribe el nombre de la ventana: ");
+        window = sc.nextLine();
+        System.out.println("Escribe el texto del boton: ");
+        btn = sc.nextLine();
+
+        frame.dispose();
+
+        Ej02P02 frame2 = new Ej02P02(window, btn);
     }
 }
