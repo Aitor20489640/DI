141d659840bfded779034d95cac635ea17398350
Aitor Rodriguez <Aitor.Gallardo.14@gmail.com>
2022-10-11 18:05:54 +0200

    Practica 3 W.I.P Logica hecha pero el metodo calculate no tiene recursividad

--- T02/Ej02/EJ03_4.java
+++ T02/Ej02/EJ03_4.java
@@ -1,9 +1,37 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class EJ03_4 {
+    public EJ03_4() {
+        JFrame JExplicacion = new JFrame("Ventana");
+        JButton btnContinue = new JButton("Continuar");
+
+        JExplicacion.setLayout(new BorderLayout(10, 10));
+        JExplicacion.add(new JLabel("Programa para convertir celsius a farenheit", SwingConstants.CENTER), BorderLayout.CENTER);
+        JExplicacion.add(btnContinue, BorderLayout.SOUTH);
+        JExplicacion.setSize(500, 500);
+        JExplicacion.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        JExplicacion.setLocationRelativeTo(null);
+        JExplicacion.setVisible(true);
+
+        btnContinue.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JExplicacion.dispose();
+
+            }
+        });
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
+        new EJ03_4();
+
+        /*Scanner sc = new Scanner(System.in);
         double celsius = 0.0;
         boolean ok;
 
@@ -18,7 +46,7 @@ public class EJ03_4 {
             }
         }while(!ok);
 
-        System.out.println("Estos son los grados en farenheit " + celsiusToFarenheit(celsius));
+        System.out.println("Estos son los grados en farenheit " + celsiusToFarenheit(celsius));*/
 
     }
 
