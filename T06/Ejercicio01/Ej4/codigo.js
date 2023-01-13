cf73f45dbf0e963ea917562e2a1b7b6074fc3484
Aitor Rodriguez <Aitor.Gallardo.14@gmail.com>
2023-01-13 17:04:40 +0100

    Ejercicio 1 Tema 6 terminado

---
+++ T06/Ejercicio01/Ej4/codigo.js
@@ -0,0 +1,2 @@
+console.log("Hola Mundo!"); //Pone por la consolo "Hola Mundo"
+console.log("Soy el primer script"); //Pone por la consola "Soy el primer script"
---
+++ T06/Ejercicio01/Ej5/codigo.js
@@ -0,0 +1,9 @@
+let mensaje = "Hola Mundo!"; // Variable donde se guarda el mensaje de aviso
+
+console.log(mensaje); //Pone por la consolo "Hola Mundo"
+console.log("Soy el primer script"); //Pone por la consola "Soy el primer script"
+
+// Modifico la variable para que se muestre otro mensaje
+mensaje = "Hola Mundo! Que fácil es incluir 'comillas simples' y \"comillas dobles\"";
+
+window.alert(mensaje) //Muestra una ventana de alerta en pantalla con el mensaje de aletra
