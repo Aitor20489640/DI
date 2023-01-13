55947427b14a9904b9a5f3c548203f72b2b2af20
Aitor Rodriguez <Aitor.Gallardo.14@gmail.com>
2023-01-13 19:07:31 +0100

    Ejercicio 2 Tema 6 terminado

---
+++ T06/Ejercicio03/Ej1/script.js
@@ -0,0 +1,5 @@
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+for (const mes of meses) {
+    console.log(mes);
+}
