5668d3db7463865a92febe31fe3b3944de645e39
Aitor Rodriguez <Aitor.Gallardo.14@gmail.com>
2023-01-13 17:19:27 +0100

    Ejercicio 2 Tema 6 terminado

---
+++ T06/Ejercicio02/script.js
@@ -0,0 +1,13 @@
+let incr = 1;
+incr++;
+let decr = 1;
+decr--;
+
+document.getElementById("p1").innerHTML = "+ : " + (2 +2)
+document.getElementById("p2").innerHTML = "- : " + (2 - 2)
+document.getElementById("p3").innerHTML = "* : " + (2 * 2)
+document.getElementById("p4").innerHTML = "/ : " + (2 / 2)
+document.getElementById("p5").innerHTML = "** : " + (2 ** 3)
+document.getElementById("p6").innerHTML = "% : " + (2 % 2)
+document.getElementById("p7").innerHTML = "++ : " + incr
+document.getElementById("p8").innerHTML = "-- : " + decr;
