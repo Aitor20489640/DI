7186f4221d49b9844273326b1647f503835f29ea
Aitor Rodriguez Gallardo <Aitor.gallardo.14@gmail.com>
2023-02-03 17:47:29 +0100

    Ejericio 6 Tema 6 hecho con adicicion de frases por el usuario

--- T06/Ejercicio06/script.js
+++ T06/Ejercicio06/script.js
@@ -1,16 +1,44 @@
+
+let frases = ["tendrás un día de alegrías y buenos momentos, disfrútalos como nunca.", "tendrás un día de alegrías y buenos momentos, disfrútalos como nunca.",
+    "concéntrate en lo que quieres lograr y ganaras. No lo olvides.", "el cielo sera tu limite, pues grandes acontecimientos te sucederán.",
+    "te sentirás feliz como un niño y veras al mundo con sus ojos.", "vivirás tu vejez con comodidades y riquezas materiales.",
+    "confía en tu suerte, que es mucha y te rodeara de prosperidad.", "no todo el mundo puede recibir las mismas cosas. Se practico.",
+    "te aguarda una larga y feliz vida.", "muy pronto seras incluido en muchas reuniones, fiestas y tertulias.",
+    "cuando busques lo que mas deseas, recuerda hacer tu mejor esfuerzo.", "tienes por delante un maravilloso día para triunfar; disfrútalo y compártelo.",
+    "hoy seras reconocido por tus dones especiales y lograras ser feliz por muchas horas.", "tu corazón estallara de alegría con la llegada de buenas noticias.",
+    "seras promovido en tu trabajo debido a tus logros y capacidades."];
+let person;
+
 async function predecirFuturo() {
-    let frases = ["Tendrás un día de alegrías y buenos momentos, disfrútalos como nunca.", "Tendrás un día de alegrías y buenos momentos, disfrútalos como nunca.",
-        "Concéntrate en lo que quieres lograr y ganaras. No lo olvides.", "El cielo sera tu limite, pues grandes acontecimientos te sucederán.",
-        "Te sentirás feliz como un niño y veras al mundo con sus ojos.", "Vivirás tu vejez con comodidades y riquezas materiales.",
-        "Confía en tu suerte, que es mucha y te rodeara de prosperidad.", "No todo el mundo puede recibir las mismas cosas. Se practico.",
-        "Te aguarda una larga y feliz vida.", "Muy pronto seras incluido en muchas reuniones, fiestas y tertulias.",
-        "Cuando busques lo que mas deseas, recuerda hacer tu mejor esfuerzo.", "Tienes por delante un maravilloso día para triunfar; disfrútalo y compártelo.",
-        "Hoy seras reconocido por tus dones especiales y lograras ser feliz por muchas horas.", "Tu corazón estallara de alegría con la llegada de buenas noticias.",
-        "Seras promovido en tu trabajo debido a tus logros y capacidades."];
     let random = Math.floor(Math.random() * frases.length);
 
+
+    if (person == null) {
+        person = window.prompt("Indica tu nombre");
+    }
+    if (person == null || person == "") {// Se comprueba si el nombre indicado esta vacio y se asigna un nombre por defecto.
+        person = "Usuario";
+    }
     document.getElementById("futuro").innerHTML = "Consultando a los espíritus…";
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    document.getElementById("futuro").innerHTML = frases[random];
+
+    await new Promise(resolve => setTimeout(resolve, 3000)); // Se espera un segundo a dar la respuesta a para darle un carácter misterioso.
+    document.getElementById("futuro").innerHTML = person + ", " + frases[random];
 }
+
+function addFrases() {
+    let frase = document.getElementById("addFrase").value;
+
+    if (frase !== "") {// Se comprueba si la frase no esta vacia
+        if (frase.match(/^[A-Za-z,.]+$/)){ // Se comprueba si la frase tiene letras
+            frases.push(frase); // Se añade la frase al array
+        } else {
+            alert("La frase debe contener caracteres");
+        }
+    } else {
+        alert("La frase no puede estar vacia");
+
+    }
+}
+
+
 
